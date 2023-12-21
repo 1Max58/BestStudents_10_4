3f2387d12144df63ba126a312158986c2a95c20e
1Max58 <mkorsun1@stu.vistula.edu.pl>
2023-12-21 12:18:15 +0200

    Writing best students to a separate file implemented

--- src/com/example/beststudents/Main.java
+++ src/com/example/beststudents/Main.java
@@ -1,7 +1,27 @@
 package com.example.beststudents;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        String s;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("students.txt"));
+             FileWriter fw = new FileWriter("students_best.txt")) {
+            while ((s = br.readLine()) != null) {
+                s = s.trim();
+                if (s.endsWith("5")) {
+                    s = s + "\r\n";
+                    fw.write(s);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e);
+        }
     }
 }
